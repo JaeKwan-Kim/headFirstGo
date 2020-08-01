226b6746fc9807ee703a96fae3ed20a25d3704bc
JaeKwan-Kim <kajki@naver.com>
2020-08-02 02:44:14 +0900

    head first go p. 15

--- src/main.go
+++ src/main.go
@@ -2,25 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"strings"
+	"reflect"
 )
 
 func main() {
-
-	fmt.Println(math.Floor(2.75))
-	fmt.Println(strings.Title("head first go"))
-	fmt.Println("Hello, \nGo!")
-	fmt.Println("Hello, \tGo!")
-	fmt.Println("Quotes:\"\"")
-	fmt.Println("Backslash:\\")
-
-	// rune 룬
-	fmt.Println('A')
-	fmt.Println('B')
-	fmt.Println('\t')
-
-	//boolean
-	fmt.Println(true)
-	fmt.Println(false)
+	fmt.Println(reflect.TypeOf(42))
+	fmt.Println(reflect.TypeOf(3.14159))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(reflect.TypeOf("Hello, Go!"))
 }
