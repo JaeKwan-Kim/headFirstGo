78b06e96be7c9ee34d77a9b4d9655f1f968cfa4c
JaeKwan-Kim <kajki@naver.com>
2020-08-02 02:28:14 +0900

    head first go p. 11

--- src/main.go
+++ src/main.go
@@ -10,4 +10,8 @@ func main() {
 
 	fmt.Println(math.Floor(2.75))
 	fmt.Println(strings.Title("head first go"))
+	fmt.Println("Hello, \nGo!")
+	fmt.Println("Hello, \tGo!")
+	fmt.Println("Quotes:\"\"")
+	fmt.Println("Backslash:\\")
 }
