7bda11aefe50b8c8eceb310fbdd9bf850dbdd52e
JaeKwan-Kim <kajki@naver.com>
2020-08-03 01:42:28 +0900

    head first go p. 23

--- src/main.go
+++ src/main.go
@@ -6,27 +6,16 @@ import (
 )
 
 func main() {
-	fmt.Println(reflect.TypeOf(42))
+	myInt := 2
+	fmt.Println(reflect.TypeOf(myInt))
+	fmt.Println(reflect.TypeOf(float64(myInt)))
 
-	//var quantity int
-	//var length, width float64
-	//var customerName string
-	//
-	//quantity = 4
-	//customerName = "Damon Cole"
-	//length, width = 1.2, 2.4
+	length := 1.2
+	width := 2
 
-	//var quantity = 4
-	//var customerName = "Damon Cole"
-	//var length, width = 1.2, 2.4
+	//length = float64(width)
+	//fmt.Println(length)
 
-	quantity := 4
-	customerName := "Damon Cole"
-	length, width := 1.2, 2.4
-
-	fmt.Println(customerName)
-	fmt.Println("has ordered", quantity, "sheets")
-	fmt.Println("each with an area of")
-	fmt.Println(length*width, "square meters")
-
+	width = int(length)
+	fmt.Println(width)
 }
