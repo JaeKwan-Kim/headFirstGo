077565feb52b8079562dc901991f32052d8e2a4f
JaeKwan-Kim <kajki@naver.com>
2020-08-02 21:07:17 +0900

    head first go p. 19

--- src/main.go
+++ src/main.go
@@ -7,7 +7,22 @@ import (
 
 func main() {
 	fmt.Println(reflect.TypeOf(42))
-	fmt.Println(reflect.TypeOf(3.14159))
-	fmt.Println(reflect.TypeOf(true))
-	fmt.Println(reflect.TypeOf("Hello, Go!"))
+
+	//var quantity int
+	//var length, width float64
+	//var customerName string
+	//
+	//quantity = 4
+	//customerName = "Damon Cole"
+	//length, width = 1.2, 2.4
+
+	var quantity = 4
+	var customerName = "Damon Cole"
+	var length, width = 1.2, 2.4
+
+	fmt.Println(customerName)
+	fmt.Println("has ordered", quantity, "sheets")
+	fmt.Println("each with an area of")
+	fmt.Println(length*width, "square meters")
+
 }
