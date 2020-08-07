4dc19022ed28ac6732d63a6f8238ffd1114d6950
JaeKwan-Kim <kajki@naver.com>
2020-08-08 01:44:32 +0900

    head first go p. 69

---
+++ src/guess.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+
+	fmt.Println("I've chosen a random number between 1 and 100")
+	fmt.Println("Can you guess it?")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Make a guess :")
+
+	for guesses := 0; guesses < 10; guesses++ {
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		input = strings.TrimSpace(input)
+		guess, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//fmt.Println(target)
+
+		if guess < target {
+			fmt.Println("Oops. Your guess was LOW.")
+		} else if guess > target {
+			fmt.Println("Oops. Your guess was HIGH.")
+		} else {
+			fmt.Println("Good job! You guessed it!")
+			break
+		}
+	}
+
+}
