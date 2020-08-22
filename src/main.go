c03a6170c67d87bb31c4d237a7e7857d150be3af
JaeKwan-Kim <0!worhks79>
2020-08-23 01:53:53 +0900

    head first go p. 96

--- src/main.go
+++ src/main.go
@@ -1,22 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func paintNeeded(width float64, height float64, be_print bool) float64 {
+func paintNeeded(width float64, height float64, be_print bool) (error, float64) {
 	area := width * height
+
+	if area < 0 || width < 0 {
+		err := errors.New("area OR width Can't")
+		return err, 0
+	}
 
 	if be_print == true {
 		fmt.Printf("\t %.2f liters needed\n", area/10.0)
 	}
 
-	return area / 10.0
+	return nil, area / 10.0
 }
 
 func main() {
 	var total float64
 
-	total += paintNeeded(4.2, 3.0, true)
-	total += paintNeeded(5.2, 3.5, true)
+	_, total = paintNeeded(4.2, 3.0, true)
+	_, total = paintNeeded(5.2, 3.5, true)
 
 	fmt.Printf("Total : %0.2f liters\n", total)
 	fmt.Printf("Total : %0.2f liters\n", total)
