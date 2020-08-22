2997b0dbb5f602c687fd51a2c6de55b0daeaeaa7
JaeKwan-Kim <0!worhks79>
2020-08-22 23:24:04 +0900

    sg commit test

--- src/main.go
+++ src/main.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
-func paintNeeded(width float64, height float64) {
+func paintNeeded(width float64, height float64, be_print bool) float64 {
 	area := width * height
-	fmt.Printf("%.2f liters needed\n", area/10.0)
+
+	if be_print == true {
+		fmt.Printf("\t %.2f liters needed\n", area/10.0)
+	}
+
+	return area / 10.0
 }
 
 func main() {
-	paintNeeded(4.2, 3.0)
-	paintNeeded(5.2, 3.5)
+	var total float64
+
+	total += paintNeeded(4.2, 3.0, true)
+	total += paintNeeded(5.2, 3.5, true)
+
+	fmt.Printf("Total : %0.2f liters\n", total)
+
 }
